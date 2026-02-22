e68689d4f2cc6e960dd79a5c6ee111688dc00ab5
cyrus104 <cyrus104@gmail.com>
2026-02-22 01:39:53 -0500

    fix: align Docker.php with reference plugin structure and fix loadData() constructor bug

--- Docker.php
+++ Docker.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * Docker
+ *
+ * @description A Docker container management plugin for RaspAP
+ * @author      RaspAP <hello@raspap.com>
+ * @license     https://github.com/RaspAP/raspap-webgui/blob/master/LICENSE
+ * @see         src/RaspAP/Plugins/PluginInterface.php
+ * @see         src/RaspAP/UI/Sidebar.php
+ */
+
 namespace RaspAP\Plugins\Docker;
 
 use RaspAP\Plugins\PluginInterface;
@@ -13,6 +23,7 @@ if (!defined('RASPI_DOCKER_CONFIG')) {
 
 class Docker implements PluginInterface
 {
+
     private string $pluginPath;
     private string $pluginName;
     private string $templateMain;
@@ -21,6 +32,12 @@ class Docker implements PluginInterface
     private string $icon;
     private string $binPath;
 
+    /**
+     * Initializes the Docker plugin
+     *
+     * @param string $pluginPath path to the plugins directory
+     * @param string $pluginName name of this plugin
+     */
     public function __construct(string $pluginPath, string $pluginName)
     {
         $this->pluginPath = $pluginPath;
@@ -30,80 +47,108 @@ class Docker implements PluginInterface
         $this->label = 'Docker';
         $this->icon = 'fab fa-docker';
         $this->binPath = '/usr/bin/docker';
-        $this->loadData();
+
+        if ($loaded = self::loadData()) {
+            $this->serviceStatus = $loaded->serviceStatus;
+        }
     }
 
+    /**
+     * Initializes Docker and creates a sidebar item
+     *
+     * @param Sidebar $sidebar an instance of the Sidebar
+     * @see src/RaspAP/UI/Sidebar.php
+     * @see https://fontawesome.com/icons
+     */
     public function initialize(Sidebar $sidebar): void
     {
-        $sidebar->addItem('Docker', $this->icon, 'plugin__Docker', 77);
+        $label    = $this->label;
+        $icon     = $this->icon;
+        $action   = 'plugin__' . $this->getName();
+        $priority = 77;
+
+        $sidebar->addItem($label, $icon, $action, $priority);
     }
 
+    /**
+     * Handles a page action by processing inputs and rendering a plugin template
+     *
+     * @param string $page the current page route
+     * @return bool true if this plugin handled the page, false otherwise
+     */
     public function handlePageAction(string $page): bool
     {
-        if (strpos($page, '/plugin__' . $this->getName()) !== 0) {
-            return false;
-        }
+        if (strpos($page, '/plugin__' . $this->getName()) === 0) {
+            $status = new \RaspAP\Messages\StatusMessage();
 
-        $status = new \RaspAP\Messages\StatusMessage();
+            if (!file_exists($this->binPath)) {
+                $msg = 'Docker binary not found. See https://docs.docker.com/engine/install/debian/ to install Docker.';
+                $status->addMessage($msg, 'warning');
+                $daemonStatus = 'inactive';
+                $containers = [];
+                $images = [];
+                $systemDf = [];
+                $dockerVersion = '';
+            } else {
+                $dockerService = new DockerService();
+                $daemonStatus = $dockerService->getDaemonStatus();
+                $containers = $dockerService->getContainers();
+                $images = $dockerService->getImages();
+                $systemDf = $dockerService->getSystemDf();
+                $dockerVersion = $dockerService->getDockerVersion();
+                $composeProjects = $dockerService->getComposeProjects();
+                $volumes = $dockerService->getVolumes();
 
-        if (!file_exists($this->binPath)) {
-            $status->addMessage('Docker binary not found. See https://docs.docker.com/engine/install/debian/ to install Docker.', 'warning');
-            $daemonStatus = 'inactive';
-            $containers = [];
-            $images = [];
-            $systemDf = [];
-            $dockerVersion = '';
-        } else {
-            $dockerService = new DockerService();
-            $daemonStatus = $dockerService->getDaemonStatus();
-            $containers = $dockerService->getContainers();
-            $images = $dockerService->getImages();
-            $systemDf = $dockerService->getSystemDf();
-            $dockerVersion = $dockerService->getDockerVersion();
-            $composeProjects = $dockerService->getComposeProjects();
-            $volumes = $dockerService->getVolumes();
-
-            if (!RASPI_MONITOR_ENABLED && isset($_POST['saveCompose'])) {
-                $project = trim($_POST['compose_project'] ?? '');
-                $yaml = $_POST['compose_yaml'] ?? '';
-                if ($project !== '' && $yaml !== '') {
-                    $dockerService->saveComposeFile($project, $yaml);
+                if (!RASPI_MONITOR_ENABLED && isset($_POST['saveCompose'])) {
+                    $project = trim($_POST['compose_project'] ?? '');
+                    $yaml = $_POST['compose_yaml'] ?? '';
+                    if ($project !== '' && $yaml !== '') {
+                        $dockerService->saveComposeFile($project, $yaml);
+                    }
                 }
             }
+
+            $this->serviceStatus = ($daemonStatus === 'active') ? 'up' : 'down';
+
+            $__template_data = [
+                'title'          => $this->label,
+                'description'    => _('A Docker container management plugin for RaspAP'),
+                'author'         => 'RaspAP',
+                'uri'            => 'https://github.com/RaspAP/',
+                'icon'           => $this->icon,
+                'serviceStatus'  => $this->serviceStatus,
+                'serviceName'    => 'docker',
+                'action'         => 'plugin__' . $this->getName(),
+                'pluginName'     => $this->getName(),
+                'daemonStatus'   => $daemonStatus,
+                'containers'     => $containers,
+                'systemDf'       => $systemDf,
+                'dockerVersion'  => $dockerVersion,
+                'images'         => $images,
+                'composeProjects' => $composeProjects ?? [],
+                'volumes'        => $volumes ?? [],
+            ];
+
+            echo $this->renderTemplate($this->templateMain, compact('status', '__template_data'));
+
+            return true;
         }
-
-        $this->serviceStatus = ($daemonStatus === 'active') ? 'up' : 'down';
-
-        $__template_data = [
-            'title'          => $this->label,
-            'description'    => _('A Docker container management plugin for RaspAP'),
-            'author'         => 'RaspAP',
-            'uri'            => 'https://github.com/RaspAP/',
-            'icon'           => $this->icon,
-            'serviceStatus'  => $this->serviceStatus,
-            'serviceName'    => 'docker',
-            'action'         => 'plugin__' . $this->getName(),
-            'pluginName'     => $this->getName(),
-            'daemonStatus'   => $daemonStatus,
-            'containers'     => $containers,
-            'systemDf'       => $systemDf,
-            'dockerVersion'  => $dockerVersion,
-            'images'         => $images,
-            'composeProjects' => $composeProjects ?? [],
-            'volumes'        => $volumes ?? [],
-        ];
-
-        echo $this->renderTemplate($this->templateMain, compact('status', '__template_data'));
-
-        return true;
+        return false;
     }
 
+    /**
+     * Renders a template from inside the plugin directory
+     *
+     * @param string $templateName name of the template (without .php extension)
+     * @param array  $__data       data to extract into the template scope
+     * @return string rendered template output
+     */
     public function renderTemplate(string $templateName, array $__data = []): string
     {
         $templateFile = $this->pluginPath . '/' . $this->getName() . '/templates/' . $templateName . '.php';
 
         if (!file_exists($templateFile)) {
-            return '';
+            return "Template file {$templateFile} not found.";
         }
 
         extract($__data);
@@ -112,11 +157,23 @@ class Docker implements PluginInterface
         return ob_get_clean();
     }
 
+    /**
+     * Persists plugin data to a temporary file
+     *
+     * @note Data is written to /tmp and cleared on reboot. Not for long-term storage.
+     * @note Plugins should avoid $_SESSION vars as these may conflict with other plugins.
+     * @return void
+     */
     public function persistData(): void
     {
         file_put_contents("/tmp/plugin__{$this->getName()}.data", serialize($this));
     }
 
+    /**
+     * Loads previously persisted plugin data
+     *
+     * @return self|null the deserialized instance, or null if none exists
+     */
     public static function loadData(): ?self
     {
         $file = "/tmp/plugin__" . self::getName() . ".data";
@@ -130,6 +187,11 @@ class Docker implements PluginInterface
         return null;
     }
 
+    /**
+     * Returns the abbreviated class name used as the plugin identifier
+     *
+     * @return string plugin name
+     */
     public static function getName(): string
     {
         return basename(str_replace('\\', '/', static::class));
